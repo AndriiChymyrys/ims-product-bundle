d0a5ccd559fc5d66346048c7739ce1d261c195ca
Andrii Chymyrys <andrii.chymyrys@gmail.com>
2022-06-15 19:12:18 +0300

    add repository publish service

---
+++ Infrastructure/Repository/ProductRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WideMorph\Ims\Bundle\ImsProductBundle\Infrastructure\Repository;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use WideMorph\Ims\Bundle\ImsProductBundle\Infrastructure\Entity\Product;
+
+/**
+ * Class ProductRepository
+ *
+ * @package WideMorph\Ims\Bundle\ImsProductBundle\Infrastructure\Repository
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, string $entityClass = Product::class)
+    {
+        parent::__construct($registry, $entityClass);
+    }
+}
